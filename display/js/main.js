2b59e992e61c29f96a06111f8e0791c2cbfa870b
Tom Garratt <tomagarratt@yahoo.co.uk>
2019-04-25 07:58:06 +0000

    Orbit time graph implimented.

--- display/js/main.js
+++ display/js/main.js
@@ -5,27 +5,45 @@ queue()
 function makeGraphs(error, MoonsOfUranus) {
     var ndx = crossfilter(MoonsOfUranus);
 
-    display_surface_temperature(ndx)
+    display_surface_temperature(ndx);
+    display_orbit_time(ndx);
 
 
     dc.renderAll();
 }
 
-function display_surface_temperature(ndx){
-    var dim = ndx.dimension(dc.pluck('Temperature'));
-    var group = dim.group();
+function display_surface_temperature(ndx) {
+    var tempDim = ndx.dimension(dc.pluck('Temperature'));
+    var tempGroup = tempDim.group();
 
     dc.barChart("#surface-temp")
         .width(400)
         .height(300)
         .margins({ top: 10, right: 50, bottom: 30, left: 50 })
-        .dimension(dim)
-        .group(group)
+        .dimension(tempDim)
+        .group(tempGroup)
         .transitionDuration(500)
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
         .elasticY(true)
         .xAxisLabel("Temperature (kelvins)")
         .yAxis().ticks(20);
-};
+}
 
+function display_orbit_time(ndx) {
+    var orbDim = ndx.dimension(dc.pluck('OrbitalTime'));
+    var orbGroup = orbDim.group();
+
+    dc.barChart("#orbit-time")
+        .width(400)
+        .height(300)
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
+        .dimension(orbDim)
+        .group(orbGroup)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .xAxisLabel("Orbital time (hours)")
+        .yAxis().ticks(20);
+}
