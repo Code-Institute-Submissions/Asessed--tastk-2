fbd3bc09052ea68f5b786eb788d6ce20b177d12c
Tom Garratt <tomagarratt@yahoo.co.uk>
2019-04-30 13:01:12 +0000

    Large section of scatter graph implimented

--- display/js/main.js
+++ display/js/main.js
@@ -5,8 +5,13 @@ queue()
 function makeGraphs(error, MoonsOfUranus) {
     var ndx = crossfilter(MoonsOfUranus);
 
+    MoonsOfUranus.forEach(function(d) {
+        d.Radius = parseInt(d.Radius);
+    })
+
     display_surface_temperature(ndx);
     display_orbit_time(ndx);
+    display_radius_temp_comparison(ndx);
 
 
     dc.renderAll();
@@ -46,4 +51,34 @@ function display_orbit_time(ndx) {
         .elasticY(true)
         .xAxisLabel("Orbital time (hours)")
         .yAxis().ticks(20);
+}
+
+function display_radius_temp_comparison(ndx) {
+    var radDim = ndx.dimension(dc.pluck('Radius'))
+    var TempDim = ndx.dimension(function(d) {
+        return [d.Radius, d.Temperature, d.Name];
+    });
+    var radiusTempGroup = TempDim.group();
+
+    var minExperience = radDim.bottom(1)[0].Radius;
+    var maxExperience = radDim.top(1)[0].Radius;
+
+    dc.scatterPlot("#radius-temp")
+        .width(800)
+        .height(400)
+        .x(d3.scale.linear().domain([minExperience, maxExperience]))
+        .y(d3.scale.ordinal())
+        
+        .brushOn(false)
+        .symbolSize(8)
+        .clipPadding(10)
+        .yAxisLabel("Temp")
+        .xAxisLabel("Radius")
+        .title(function(d) {
+            return d.key[2];
+        })
+        .dimension(TempDim)
+        .group(radiusTempGroup)
+        .margins({ top: 10, right: 50, bottom: 75, left: 75 })
+        .yAxis().ticks(10);
 }
